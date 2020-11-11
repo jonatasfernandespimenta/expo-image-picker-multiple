5a24302a036b8b5e5f25b41b8228497d680fa5a9
Jônatas Fernandes Pimenta <51496759+jonatasfernandespimenta@users.noreply.github.com>
2020-11-11 10:55:57 -0300

    Update ImageTile.js

--- src/ImageTile.js
+++ src/ImageTile.js
@@ -7,22 +7,24 @@ import {
 } from 'react-native';
 
 const {width} = Dimensions.get('window');
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 class ImageTile extends React.PureComponent {
   render() {
-    const { item, index, selected, selectImage, selectedItemNumber, renderSelectedComponent } = this.props;
+    const { error, item, index, selected, selectImage, selectedItemNumber, renderSelectedComponent } = this.props;
     if (!item) return null;
     return (
       <TouchableHighlight
-        style={{ opacity: selected ? 0.5 : 1 }}
+        style={{ opacity: error ? 0.7 : selected ? 0.5 : 1, backgroundColor: 'red' }}
         underlayColor='transparent'
         onPress={() => selectImage(index)} >
-        <View style={{ position: 'relative' }}>
+        <View style={{ position: 'relative' }}
+      >
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <ImageBackground
-              style={{ width: width / 4, height: width / 4 }}
+              style={{ width: width / 4, height: width / 4, justifyContent: 'center', alignItems: 'center' }}
               source={{ uri: item.uri }} >
               {selected && renderSelectedComponent && renderSelectedComponent(selectedItemNumber)}
+              {error ? <Icon name='warning' size={40} color={'white'} /> : null}
             </ImageBackground>
           </View>
         </View>
